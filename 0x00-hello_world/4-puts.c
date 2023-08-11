20bc71ca0e8f880bffce83153fb1996b81f53254
Cyphyr0 <itsaitsh@gmail.com>
2023-08-10 19:34:20 -0700

    hello c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main – start - Entry point
+ *
+ * Return: Always 0 (means Success)
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
